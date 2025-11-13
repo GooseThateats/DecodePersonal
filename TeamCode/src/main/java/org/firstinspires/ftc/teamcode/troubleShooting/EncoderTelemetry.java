24926dcf18e9d508d97a6c4544789a51854c44c6
guosenruan <gruan3@cps.edu>
2025-11-12 18:09:35 -0600

    oct 28

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/troubleShooting/EncoderTelemetry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/troubleShooting/EncoderTelemetry.java
@@ -23,75 +23,48 @@ import java.util.ArrayList;
 @TeleOp
 public final class EncoderTelemetry extends LinearOpMode {
 
-    private Pose2d beginPose;
-    private MecanumDrive drive;
-    private CameraSystem camera;
-    private Firecracker rightFirecracker;
-    private Firecracker leftFirecracker;
-    private Inhaler inhaler;
-    private Feeder leftFeeder;
-    private Feeder rightFeeder;
-    private LED leftLight;
-    private LED middleLight;
-    private LED rightLight;
-    private ColorSensorCode leftColor;
-    private ColorSensorCode rightColor;
-    private DriveMotorTest vroom;
+    //enter how many motors you have
 
-    private Hammer hammer;
+    private int numberOfMotors = 10;
 
-    final double FEED_TIME_SECONDS = 0.80; //The feeder servos run this long when a shot is requested.
-    final double STOP_SPEED = 0.0; //We send this power to the servos when we want them to stop.
-    final double FULL_SPEED = 1.0;
+    //enter how many crservos you have
 
-    final double LAUNCHER_CLOSE_TARGET_VELOCITY = 1200; //in ticks/second for the close goal.
-    final double LAUNCHER_CLOSE_MIN_VELOCITY = 1175; //minimum required to start a shot for close goal.
+    private int numberOfCRServos = 10;
 
-    final double LAUNCHER_FAR_TARGET_VELOCITY = 1350; //Target velocity for far goal
-    final double LAUNCHER_FAR_MIN_VELOCITY = 1325; //minimum required to start a shot for far goal.
+    //enter how many servos you have
 
-    double launcherTarget = LAUNCHER_CLOSE_TARGET_VELOCITY; //These variables allow
-    double launcherMin = LAUNCHER_CLOSE_MIN_VELOCITY;
+    private int numberOfServos = 10;
 
-    final double LEFT_POSITION = 0.2962; //the left and right position for the diverter servo
-    final double RIGHT_POSITION = 0;
+    //name all the names based on their deviceName, add more if needed
+    private final String[] MOTORNAMES = {"motor1", "motor2", "motor3", "motor4"};
+
+    private final String[] CRSERVONAMES = {"crServo1"};
+
+    private final String[] SERVONAMES = {"servo1"};
+
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        beginPose                = new Pose2d(0, 0, 0);
-        drive              = new MecanumDrive(hardwareMap, beginPose);
-        camera             = new CameraSystem(hardwareMap);
-        rightFirecracker    =  new Firecracker(hardwareMap, "right_launcher");
-        leftFirecracker     =  new Firecracker(hardwareMap, "left_launcher");
-        inhaler                 = new Inhaler(hardwareMap, "intake");
-        leftFeeder               = new Feeder(hardwareMap, "left_feeder");
-        rightFeeder              = new Feeder(hardwareMap, "right_feeder");
-        leftLight                   = new LED(hardwareMap, "left_light");
-        middleLight                 = new LED(hardwareMap, "middle_light");
-        rightLight                  = new LED(hardwareMap, "right_light");
-        leftColor       = new ColorSensorCode(hardwareMap, "left_color_sensor");
-        rightColor      = new ColorSensorCode(hardwareMap, "right_color_sensor");
-        vroom            = new DriveMotorTest(hardwareMap);
+        ArrayList<Motors> dcMotorList = new ArrayList<Motors>();
+        ArrayList<ContinuousRotationServos> crServoList = new ArrayList<ContinuousRotationServos>();
+        ArrayList<Servos> servoList = new ArrayList<Servos>();
 
-        ArrayList<DcMotorEx> dcMotorList = new ArrayList<DcMotorEx>();
-        ArrayList<CRServo> crServoList = new ArrayList<CRServo>();
-        ArrayList<Servo> servoList = new ArrayList<Servo>();
+        for(int i = 0; i<numberOfMotors; i++){
+            dcMotorList.add(new Motors(hardwareMap, MOTORNAMES[i]));
+        }
 
+        for(int i = 0; i<numberOfCRServos; i++){
+            crServoList.add(new ContinuousRotationServos(hardwareMap, CRSERVONAMES[i]));
+        }
 
+        for(int i = 0; i<numberOfServos; i++){
+            servoList.add(new Servos(hardwareMap, SERVONAMES[i]));
+        }
 
-        boolean reverse = true;
-        boolean notReverse = false;
-        int detectedID;
-
-        //adjust reverse if needed
-        leftFeeder.initialize(reverse);
-        rightFeeder.initialize(notReverse);
-        leftFirecracker.initialize(notReverse);
-        rightFirecracker.initialize(reverse);
-        inhaler.initialize(reverse);
-        vroom.initialize(true);
-        //camera.cameraOn();
+        for(int i = 0; i<numberOfMotors; i++){
+            dcMotorList.get(i).initialize();
+        }
 
 
         waitForStart();
@@ -99,17 +72,17 @@ public final class EncoderTelemetry extends LinearOpMode {
         while(opModeIsActive()) {
             //encoder values for motors
             for(int i = 0; i<dcMotorList.size(); i ++){
-                telemetry.addData("Motor" + (i+1) + "encoder ticks: ",dcMotorList.get(i).getCurrentPosition());
+                telemetry.addData("Motor" + (i+1) + "encoder ticks: ",dcMotorList.get(i).getCurrentEncoderPosition());
                 telemetry.update();
             }
-
+            //Power values of Continuous rotational Servos
             for(int i = 0; i<crServoList.size(); i++){
-                telemetry.addData("crServo" + (i+1) + "power: ", crServoList.get(i).getPower());
+                telemetry.addData("crServo" + (i+1) + "power: ", crServoList.get(i).getCurrentPower());
                 telemetry.update();
             }
-
+            //Position values of every servos
             for(int i = 0; i<servoList.size(); i++){
-                telemetry.addData("Servo " + (i+1) + " position: ", servoList.get(i).getPosition());
+                telemetry.addData("Servo " + (i+1) + " position: ", servoList.get(i).getCurrentServoPosition());
             }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/troubleShooting/Motors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/troubleShooting/Motors.java
@@ -9,50 +9,21 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 public class Motors {
-    private DcMotorEx firecracker = null;
-    double LAUNCHER_CLOSE_TARGET_VELOCITY = 1200; //in ticks/second for the close goal.
-    final double LAUNCHER_CLOSE_MIN_VELOCITY = 1175; //minimum required to start a shot for close goal.
-
-    final double LAUNCHER_FAR_TARGET_VELOCITY = 1350; //Target velocity for far goal
-    final double LAUNCHER_FAR_MIN_VELOCITY = 1325; //minimum required to start a shot for far goal.
-
+    private DcMotorEx motor = null;
 
     public Motors(HardwareMap hardwareMap, String deviceName) {
         // Initialize arm motor
-        firecracker = hardwareMap.get(DcMotorEx.class, deviceName);
+        motor = hardwareMap.get(DcMotorEx.class, deviceName);
 
     }
 
-    public void initialize(boolean reverse){
-        if(reverse){
-            firecracker.setDirection(DcMotorSimple.Direction.REVERSE); // Adjust if necessary
-        }
-        firecracker.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        firecracker.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        firecracker.setZeroPowerBehavior(BRAKE);
-        firecracker.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(300, 0, 0, 10));
-        firecracker.setPower(0);
-
-    }
-    public void setTargetVelocity(double velocity){
-        LAUNCHER_CLOSE_TARGET_VELOCITY = velocity;
-    }
-    public void crackDaFire(){
-        firecracker.setVelocity(LAUNCHER_CLOSE_TARGET_VELOCITY);
+    public void initialize(){
+        motor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        motor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
     }
 
-    public void crackBigBoyFire(){
-        firecracker.setVelocity(LAUNCHER_FAR_TARGET_VELOCITY + 100);
+    public double getCurrentEncoderPosition(){
+        return motor.getCurrentPosition();
     }
-
-    public void ceaseFire(){
-        firecracker.setVelocity(0);
-    }
-
-    public double getCurrentVelocity(){
-        return firecracker.getVelocity();
-    }
-
-
 
 }
